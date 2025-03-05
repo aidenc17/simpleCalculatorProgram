f01e867b24c7f7498ef634ee174002bc2bcee047
aiden cox <cox.378@wright.edu>
2025-03-05 15:07:54 -0500

    simple calc using c++ and switch case

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "******************* CALCULATOR *******************\n";
+
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Result: " << result << '\n';
+            break;
+
+        case '-':
+            result = num1 - num2;
+            std::cout << "Result: " << result << '\n';
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Result: " << result << '\n';
+            break;
+        case '/':
+            result = num1 / num2;
+            std::cout << "Result: " << result << '\n';
+            break;
+        default:
+            std::cout << "Invalid operation.\n";
+    }
+    std::cout << "***************************************************";
+
+
     return 0;
 }
